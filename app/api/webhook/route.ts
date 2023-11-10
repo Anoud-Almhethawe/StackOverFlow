aeae3b4758069a9ccc36c5d6b7eae491434abe0a
anoud <almhethawe.anoud@gmail.com>
2023-11-10 14:27:39 +0200

    rewrite webhook route

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,12 +3,10 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
-// import { from } from "svix/dist/openapi/rxjsStub";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  // TODO add WEBHOOK_SECRET too env.local after making endpoint
   const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -34,7 +32,7 @@ export async function POST(req: Request) {
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
+  // Create a new SVIX instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
@@ -53,9 +51,7 @@ export async function POST(req: Request) {
     });
   }
 
-  // Get the ID and type
   const eventType = evt.type;
-  console.log(eventType);
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
@@ -102,5 +98,5 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
-  return new Response("", { status: 200 });
+  return NextResponse.json({ message: "OK" });
 }
