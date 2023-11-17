d328aa388ecd4e9765670a64c9cd22a411e8acc4
anoud <almhethawe.anoud@gmail.com>
2023-11-17 11:38:02 +0200

    mid

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,21 @@
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 
-import { authMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook"],
-  ignoredRoutes: ["/api/webhook"],
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
