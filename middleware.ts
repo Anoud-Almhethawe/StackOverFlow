fc305519a08da366164e629676849967f5b720b9
anoud <almhethawe.anoud@gmail.com>
2023-11-17 10:55:42 +0200

    trying1

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
-import { authMiddleware } from "@clerk/nextjs";
-
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
+
+import { authMiddleware } from "@clerk/nextjs/server";
+
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhook"],
