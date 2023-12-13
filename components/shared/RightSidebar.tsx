04dfde9ab23d72cfd6961f98c29360e10d60b013
anoud <almhethawe.anoud@gmail.com>
2023-12-13 12:18:55 +0200

    error correction

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -19,38 +19,40 @@ const RightSidebar = async () => {
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
         <div className="mt-7 flex w-full flex-col gap-[30px] ">
-          {hotQuestions.map(question => (
-            <Link
-              href={`/question/${question._id}`}
-              key={question._id}
-              className="flex cursor-pointer items-center justify-between gap-7"
-            >
-              <p className="body-medium text-dark500_light700">
-                {question.title}
-              </p>
-              <Image
-                src="/assets/icons/chevron-right.svg"
-                alt="chevron-right"
-                width={20}
-                height={20}
-                className="invert-colors"
-              />
-            </Link>
-          ))}
+          {hotQuestions &&
+            hotQuestions.map(question => (
+              <Link
+                href={`/question/${question._id}`}
+                key={question._id}
+                className="flex cursor-pointer items-center justify-between gap-7"
+              >
+                <p className="body-medium text-dark500_light700">
+                  {question.title}
+                </p>
+                <Image
+                  src="/assets/icons/chevron-right.svg"
+                  alt="chevron-right"
+                  width={20}
+                  height={20}
+                  className="invert-colors"
+                />
+              </Link>
+            ))}
         </div>
       </div>
       <div className="mt-16">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
         <div className="mt-7 flex flex-col gap-4">
-          {popularTags.map(tag => (
-            <RenderTag
-              key={tag._id}
-              _id={tag._id}
-              name={tag.name}
-              totalQuestions={tag.numberOfQuestions}
-              showCount
-            />
-          ))}
+          {popularTags &&
+            popularTags.map(tag => (
+              <RenderTag
+                key={tag._id}
+                _id={tag._id}
+                name={tag.name}
+                totalQuestions={tag.numberOfQuestions}
+                showCount
+              />
+            ))}
         </div>
       </div>
     </section>
