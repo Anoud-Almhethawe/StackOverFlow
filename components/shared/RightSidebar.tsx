9d77697db159f65ae4648ec9c1f65e8b9d24ebaf
anoud <almhethawe.anoud@gmail.com>
2023-12-13 11:56:17 +0200

    right side bar done

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,46 +2,26 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.action";
 
-const RightSidebar = () => {
-  const hotQuestions = [
-    {
-      _id: "1",
-      title:
-        "Would it be appropriate to point out an error in another paper during a referee report?",
-    },
-    { _id: "2", title: "How can an airconditioning machine exist?" },
-    {
-      _id: "3",
-      title: "Interrogated every time crossing UK Border as citizen",
-    },
-    { _id: "4", title: "Low digit addition generator" },
-    {
-      _id: "5",
-      title: "What is an example of 3 numbers that do not make up a vector?",
-    },
-  ];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
 
-  const popularTags = [
-    { _id: "1", name: "Next.js", totalQuestions: 20152 },
-    { _id: "1", name: "React.js", totalQuestions: 18493 },
-    { _id: "1", name: "Node.js", totalQuestions: 16269 },
-    { _id: "1", name: "Python", totalQuestions: 15121 },
-    { _id: "1", name: "Microsoft Azure", totalQuestions: 9429 },
-  ];
+  const popularTags = await getTopPopularTags();
 
   return (
     <section
       className="background-light900_dark200 light-border custom-scrollbar  
-    sticky right-0 top-0 flex h-screen w-[350px] flex-col  
-    overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden"
+    shadow-light-300 sticky right-0 top-0 flex h-screen w-[350px]  
+    flex-col overflow-y-auto border-l p-6 pt-36 dark:shadow-none max-xl:hidden"
     >
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
         <div className="mt-7 flex w-full flex-col gap-[30px] ">
           {hotQuestions.map(question => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
@@ -67,7 +47,7 @@ const RightSidebar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
