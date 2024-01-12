cc144e6a4f9e6fb527257f9f48256ab5550e855a
anoud <almhethawe.anoud@gmail.com>
2024-01-12 23:45:11 +0200

    Jobs route

---
+++ app/(root)/jobs/page.tsx
@@ -0,0 +1,17 @@
+import NoResult from "@/components/shared/NoResult";
+import React from "react";
+
+const page = () => {
+  return (
+    <NoResult
+      title="🚧 Under Construction - Stay Tuned for the Grand Unveiling! 🚀
+
+   "
+      description=" This page is a work in progress as we curate the ultimate job-finding experience for you. We're busy crafting a seamless and exciting platform that will revolutionize your job search. While we add the finishing touches, get ready to embark on an adventure where passion meets profession.💡"
+      link="/ask-question"
+      label="Ask Question"
+    />
+  );
+};
+
+export default page;
